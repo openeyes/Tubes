c89bac2ba4060534c9318f69cc6129fd4e2a51d7
Ernest Elikem <ernestelikem@gmail.com>
2012-01-16 17:27:38 +0000

    Updated DatasetFormStep1 html field options and values for surgery date

--- protected/models/DatasetFormStep1.php
+++ protected/models/DatasetFormStep1.php
@@ -1,10 +1,11 @@
 <?php
+//Default Timezone
+date_default_timezone_set('Europe/London');
 /**
  * Patient Demographics
  * @author jamie
  */
 class DatasetFormStep1 extends CFormModel {
-	
 	public $hospital_number;
 	public $pt_last_name;
 	public $pt_first_name;
@@ -15,7 +16,7 @@ class DatasetFormStep1 extends CFormModel {
 	public $ethnicity;
 	public $pt_part_of_study;
 	public $study_name;
-
+	//public $defaultTimezone="date_default_timezone_set('Europe/London')";//Ernest
 	private $_scenario='wizard';
 	
 	/**
@@ -29,42 +30,83 @@ class DatasetFormStep1 extends CFormModel {
 		return array(
 					'hospital_number'=>array(
 						'type' => 'text',
+						'size'=>50,
+						'maxlength'=>50,
 					),
+					
 					'pt_last_name'=>array(
 						'label' => 'Last Name',
 						'type' => 'text',
+						'size'=>50,
+						'maxlength'=>50,
 					),
 					'pt_first_name'=>array(
 						'label' => 'First Name',
 						'type' => 'text',
+						'size'=>50,
+						'maxlength'=>50,
 					),
+					
 					'surg_op_date'=>array(
-						'label' => 'Surgery Date',
+						'label' => 'Surgery Date (DD-MM-YYYY)',//Updated Label:Ernest
 						'type' => 'ext.my97DatePicker.JMy97DatePicker',
+						'htmlOptions'=>array(
+							'value'=>$model->surg_op_date = date('d-m-Y') ,//added by Ernest
+							'readonly'=>'readonly',
+							'style'=>'width:330px;vertical-align:top',
+							'class'=>'date',		
+											),
 						'options' => array(
 							'dateFmt'=>'dd-MM-yyyy',
+							'defaultDate'=>$model->surg_op_date,
+							'showOn'=>'button', //options: 'focus', 'button', 'both'
+							'buttonText'=>Yii::t('ui','Select form calendar'), 
+							'buttonImage'=>Yii::app()->request->baseUrl.'/images/calendar2.png', 
+							'buttonImageOnly'=>true,
+							'defaultDate'=>$model->surg_op_date,
 						),
 					),
+					
+
 					'pt_dob'=>array(
-						'label' => 'Date of Birth',
+						'label' => 'Date of Birth (DD-MM-YYYY)',//Changed Label:Ernest
 						'type' => 'ext.my97DatePicker.JMy97DatePicker',
+						'htmlOptions'=>array(
+						'style'=>'width:330px;vertical-align:top',
+						'onchange'=>'calcAge()',
+						),
 						'options' => array(
-							'dateFmt'=>'dd-MM-yyyy',
+						'dateFmt'=>'dd-MM-yyyy',
+						'showOn'=>'button', // options:'focus', 'button', 'both'
+							'buttonText'=>Yii::t('ui','Select form calendar'), 
+							'buttonImage'=>Yii::app()->request->baseUrl.'/images/calendar2.png', 
+							'buttonImageOnly'=>true,
+							'defaultDate'=>$model->pt_dob,
 						),
 					),
+					
 					'pt_age'=>array(
 						'label' => 'Age',
 						'type' => 'text',
+						'size'=>50,
+						'maxlength'=>50,
+						'readonly'=>'readonly',
+						
 					),
+					
 					'pt_sex'=>array(
 						'label' => 'Gender',
 						'type' => 'dropdownlist',
+						'style'=>'width:330px;vertical-align:top',
     				'items' => ZHtml::enumItem(Dataset::model(),  'pt_sex'),
     				'prompt' => 'Please select:',
+					
 					),
+
 					'ethnicity'=>array(
 						'type' => 'dropdownlist',
     				'items' => ZHtml::enumItem(Dataset::model(),  'ethnicity'),
+					'style'=>'width:330px;vertical-align:top',
     				'prompt' => 'Please select:',
 					),
 					'pt_part_of_study'=>array(
@@ -73,6 +115,8 @@ class DatasetFormStep1 extends CFormModel {
 					),
 					'study_name'=>array(
 						'type' => 'text',
+						'size'=>50,
+						'maxlength'=>50,
 					),
 				);
 	}
