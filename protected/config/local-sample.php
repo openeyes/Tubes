b4ddf5cc9ff9d378e07d66d4efd8f500a5386348
Jamie Neil <jamie.neil@gmail.com>
2012-01-20 17:50:23 +0000

    Clean up config (separate local) Fix yiic settings Add db dump to migrations folder for reference

---
+++ protected/config/local-sample.php
@@ -0,0 +1,15 @@
+<?php
+
+return array(
+	'components'=>array(
+		'db'=>array(
+			'connectionString' => 'mysql:host=localhost;dbname=tubes',
+			'username' => 'root',
+			'password' => '',
+		),
+	),
+	'params'=>array(
+		'adminEmail'=>'jamie.neil@openeyes.org.uk',
+		'consultantEmail'=>'jamie.neil@openeyes.org.uk'
+	),
+);
