47f39ea1332bfc13310ff95fca3a06a08649bb5d
Jamie Neil <jamie@elearning.(none)>
2012-01-23 18:11:26 +0000

    fix console config

--- protected/config/console.php
+++ protected/config/console.php
@@ -2,17 +2,21 @@
 
 // This is the configuration for yiic console application.
 // Any writable CConsoleApplication properties can be configured here.
-return array(
-	'basePath'=>dirname(__FILE__).DIRECTORY_SEPARATOR.'..',
-	'name'=>'My Console Application',
-	// application components
-	'components'=>array(
-		'db'=>array(
-			'connectionString' => 'mysql:host=localhost;dbname=tubes',
-			'emulatePrepare' => true,
-			'username' => 'root',
-			'password' => '',
-			'charset' => 'utf8',
+return CMap::mergeArray(
+        array(
+		'basePath'=>dirname(__FILE__).DIRECTORY_SEPARATOR.'..',
+		'name'=>'My Console Application',
+		// application components
+		'components'=>array(
+			'db'=>array(
+				'connectionString' => 'mysql:host=localhost;dbname=tubes',
+				'emulatePrepare' => true,
+				'username' => 'root',
+				'password' => '',
+				'charset' => 'utf8',
+			),
 		),
-	),
+        ),
+        require(dirname(__FILE__).'/local.php')
 );
+
